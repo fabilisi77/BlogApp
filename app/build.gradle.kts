f2881ee01afb2dfabdc12fd0a7379d4b49402b04
Fabian <fabian_carrazan@hotmail.com>
2023-08-30 13:06:41 -0300

    Package Architecture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -44,6 +46,27 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-firestore-ktx:24.7.1")
     implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+    implementation ("'com.google.firebase:firebase-crashlytics-ktx'")
+    implementation ("com.google.firebase:firebase-analytics-ktx")
+
+    //Navigation
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.5.1")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.5.1")
+
+    //utils
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    implementation("com.github.bumptech.glide:compiler:4.11.0")
+
+    //ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+    //livedata
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
+
+    //playservice coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-service:1.1.1")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -1,77 +1,14 @@
 package com.example.blogapp
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
-import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var imageView: ImageView
-    private val REQUEST_IMAGE_CAPTURE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnTakePicture = findViewById<Button>(R.id.btn_take_picture)
-
-        imageView = findViewById(R.id.imageView)
-        btnTakePicture.setOnClickListener {
-            dispatcherTakePictureIntent()
-        }
-
-    }
-
-    private fun dispatcherTakePictureIntent() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-
-        } catch (e: ActivityNotFoundException) {
-            Toast.makeText(this, "No se encontró nnguna cámara", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            imageView.setImageBitmap(imageBitmap)
-            upLoadPicture(imageBitmap)
-        }
-    }
-
-    private fun upLoadPicture(bitmap: Bitmap){
-        val storageRef = FirebaseStorage.getInstance().reference
-        val imageRef = storageRef.child("image.jpg")
-        val baos = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-        val data = baos.toByteArray()
-        val uploadTask = imageRef.putBytes(data)
-
-        uploadTask.continueWithTask{ task ->
-            if(!task.isSuccessful){
-                task.exception?.let {exception ->
-                    throw exception
-                }
-            }
-            imageRef.downloadUrl
-
-        }.addOnCompleteListener {task ->
-        if(task.isSuccessful) {
-            val downloadUrl = task.result.toString()
-            Log.d("Storage","uploadPictureURL: $downloadUrl")
-        }
-
-        }
     }
 }
+
